a2fa1ba342adfbd113d32cb12e35d07d325f642e
lilyrisa <bvminh101299@gmail.com>
2026-01-09 20:42:37 +0700

    fix

--- resources/views/layout_old.blade.php
+++ resources/views/layout_old.blade.php
@@ -9,8 +9,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    {{-- @viteReactRefresh
-    @vite('resources/js/react/main.jsx') --}}
+    @viteReactRefresh
+    @vite('resources/js/react/main.jsx')
 
     @yield('style')
 </head>
@@ -26,7 +26,7 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
-<script src="{{ asset('js/app.js') }}"></script>
+{{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 
 
 
